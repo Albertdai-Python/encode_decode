ce2c37b1cc473aa84e5377334fa6ef42274f0383
Albertdai-Pytho <84005182+Albertdai-Python@users.noreply.github.com>
2021-08-20 03:10:39 +0000

    fixed some errors

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 def cipher_str(code_str:str, key_str:str):
+	while len(code_str) > len(key_str):
+		key_str *= 2
 	code_list = [str(bin(ord(i)))[2:] for i in code_str]
 	key_list = [str(bin(ord(i)))[2:] for i in key_str]
 	for i in range(len(code_list)):
-		if len(code_list[i]) > len(key_list[i]):
+		while len(code_list[i]) > len(key_list[i]):
 			key_list[i] *= 2
 	cipher_list = []
 	for i in range(len(code_list)):
@@ -14,15 +16,17 @@ def cipher_str(code_str:str, key_str:str):
 	cipher_str = "".join(str(b) for b in cipher_list)
 	return cipher_str
 def decipher_str(code_str:str, key_str:str):
+	while len(code_str) > len(key_str):
+		key_str += key_str
 	code_list = code_str.split("/")
 	code_list.pop()
 	key_list = [str(bin(ord(i)))[2:] for i in key_str]
-	for i in range(len(key_list)):
-		if len(code_list[i]) > len(key_list[i]):
+	for i in range(len(code_list)):
+		while len(code_list[i]) > len(key_list[i]):
 			key_list[i] *= 2
 	decipher_list = []
 	temp_decipher_list = []
-	for i in range(len(key_list)):
+	for i in range(len(code_list)):
 		temp_code_list = [int(s) for s in code_list[i]]
 		temp_key_list = [int(s) for s in key_list[i]]
 		for j in range(len(code_list[i])):
@@ -33,6 +37,6 @@ def decipher_str(code_str:str, key_str:str):
 		decipher_list[i] = chr(int(decipher_list[i],2))
 	decipher_str = "".join(str(i) for i in decipher_list)
 	return decipher_str
-#print(decipher_str(cipher_str("123qwe", "123qwe"), "123qwe"))
-print(cipher_str("123qwe", "123qwe"))
-print(decipher_str("000000/001011/000001/0000011/0000011/0011100/", "1sdrty"))
+print(decipher_str(cipher_str("i am a good boy", "123qwe"), "123qwe"))
+#print(cipher_str("12wew3qwe", "123qwe"))
+#print(decipher_str("000000/001011/000001/0000011/0000011/0011100/", "1j7e.;"))
